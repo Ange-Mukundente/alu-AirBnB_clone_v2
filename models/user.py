fc864b798f02cf026e2f457c8e8a6a3c7d60b955
renoir01 <r.kaze@alustudent.com>
2024-01-29 23:08:41 -0800

    user update

--- models/user.py
+++ models/user.py
@@ -16,3 +16,23 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="all, delete")
     reviews = relationship("Review", backref="user", cascade="all, delete")
+if __name__ == "__main__":
+    from models import storage
+    
+    # Test create User with email, password, first_name, and last_name via the console
+    user_all_attributes = User(email="a@a.com", password="pwd", first_name="fn", last_name="ln")
+    storage.new(user_all_attributes)
+    storage.save()
+    print(f"Created User with all attributes: {user_all_attributes}")
+
+    # Test create User with email and password via the console
+    user_email_password = User(email="a@a.com", password="pwd")
+    storage.new(user_email_password)
+    storage.save()
+    print(f"Created User with email and password: {user_email_password}")
+
+    # List all User in MySQL (created outside the program)
+    all_users = storage.all(User)
+    print("All Users in MySQL:")
+    for user_id, user in all_users.items():
+        print(user)
